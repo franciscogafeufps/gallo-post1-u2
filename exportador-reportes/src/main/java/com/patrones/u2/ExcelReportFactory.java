a37c9d985b209f0dd9fe35dd161452f822d133bc
franciscogafeufps <luisfranciscogf@ufps.edu.co>
2026-09-11 23:15:16 -0500

    feat(creacional): implementar familia de productos y fabricas para PDF, Excel y HTML

---
+++ exportador-reportes/src/main/java/com/patrones/u2/ExcelReportFactory.java
@@ -0,0 +1,7 @@
+// ExcelReportFactory.java
+package com.patrones.u2;
+
+public class ExcelReportFactory implements ReportFormatFactory {
+    public ReportBody createBody()               { return new ExcelReportBody(); }
+    public ReportHeaderFooter createHeaderFooter() { return new ExcelHeaderFooter(); }
+}
---
+++ exportador-reportes/src/main/java/com/patrones/u2/HtmlReportFactory.java
@@ -0,0 +1,7 @@
+// HtmlReportFactory.java
+package com.patrones.u2;
+
+public class HtmlReportFactory implements ReportFormatFactory {
+    public ReportBody createBody()               { return new HtmlReportBody(); }
+    public ReportHeaderFooter createHeaderFooter() { return new HtmlHeaderFooter(); }
+}
